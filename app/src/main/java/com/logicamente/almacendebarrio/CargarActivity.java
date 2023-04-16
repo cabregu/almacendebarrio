2a0e3767fd5315e65f0b4b48e309bf45f2366c59
Cristian Abregu <cristian.g.abregu@gmail.com>
2023-04-16 14:42:08 -0300

    lectura de ean13b 236

--- app/src/main/java/com/logicamente/almacendebarrio/CargarActivity.java
+++ app/src/main/java/com/logicamente/almacendebarrio/CargarActivity.java
@@ -1,36 +1,49 @@
 package com.logicamente.almacendebarrio;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class CargarActivity extends AppCompatActivity {
-
     private Button buttoncamera;
-    private CameraFragment camfragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cargar);
-
         buttoncamera = findViewById(R.id.button_scan);
-
         buttoncamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                CameraFragment cameraFragment = new CameraFragment();
-                fragmentManager.beginTransaction()
-                        .add(R.id.container, cameraFragment)
-                        .commit();
-
+                IntentIntegrator integrator = new IntentIntegrator(CargarActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.EAN_13);
+                integrator.setPrompt("Escanea el código de barras");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.setOrientationLocked(true);
+                integrator.initiateScan();
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Escaneo cancelado", Toast.LENGTH_LONG).show();
+            } else {
+                String barcode = result.getContents();
+                Toast.makeText(this, "Código de barras: " + barcode, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 }
